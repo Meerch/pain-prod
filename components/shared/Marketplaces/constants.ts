e7eb580933f4eb20de8c9472a5a567feecc7da68
Meerch <khukhriansk333@gmail.com>
2022-10-06 14:35:08 +0300

    up quality of social links icons

--- components/shared/Marketplaces/constants.ts
+++ components/shared/Marketplaces/constants.ts
@@ -1,16 +1,16 @@
 export const marketplaces = [
     {
-        name: 'Other1',
+        name: 'Marketplace1',
         href: 'https://opensea.io',
         icon: '/images/marketplaces/marketplace-1-4x.png'
     },
     {
-        name: 'Other2',
+        name: 'Marketplace2',
         href: 'https://opensea.io',
         icon: '/images/marketplaces/marketplace-2-4x.png'
     },
     {
-        name: 'Other3',
+        name: 'Marketplace3',
         href: 'https://opensea.io',
         icon: '/images/marketplaces/marketplace-3-4x.png'
     },
