33af3530a4185c29aafa73a325d1d99dadb27088
Meerch <khukhriansk333@gmail.com>
2022-10-06 14:34:23 +0300

    update version for script action deploy

--- components/shared/Marketplaces/constants.ts
+++ components/shared/Marketplaces/constants.ts
@@ -2,17 +2,17 @@ export const marketplaces = [
     {
         name: 'Other1',
         href: 'https://opensea.io',
-        icon: '/images/marketplaces/marketplace-1.png'
+        icon: '/images/marketplaces/marketplace-1-4x.png'
     },
     {
         name: 'Other2',
         href: 'https://opensea.io',
-        icon: '/images/marketplaces/marketplace-2.png'
+        icon: '/images/marketplaces/marketplace-2-4x.png'
     },
     {
         name: 'Other3',
         href: 'https://opensea.io',
-        icon: '/images/marketplaces/marketplace-3.png'
+        icon: '/images/marketplaces/marketplace-3-4x.png'
     },
 
 ]
--- components/shared/SocialLinks/constants.ts
+++ components/shared/SocialLinks/constants.ts
@@ -1,16 +1,16 @@
 export const links = [
     {
-        icon: '/images/social-links/twitter.png',
+        icon: '/images/social-links/twitter-4x.png',
         name: 'twitter',
         href: 'https://twitter.com',
     },
     {
-        icon: '/images/social-links/discord.png',
+        icon: '/images/social-links/discord-4x.png',
         name: 'discord',
         href: 'https://discord.com',
     },
     {
-        icon: '/images/social-links/telegram.png',
+        icon: '/images/social-links/telegram-4x.png',
         name: 'telegram',
         href: 'https://t.me',
     },
