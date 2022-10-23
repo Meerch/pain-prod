7dc29bc5f33f83f99781c963b8b659f10fcb9471
Meerch <khukhriansk333@gmail.com>
2022-10-23 10:43:34 +0300

    add 2 popups: mint and success, and init redux-toolkit, and create custom hook which detecting isMobile and isDesktop

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,12 +1,45 @@
 import styles from './Header.module.scss'
 import {SocialLinks} from "../shared/SocialLinks";
 import {Marketplaces} from "../shared/Marketplaces";
+import {ConnectButton, useAccountModal, useChainModal, useConnectModal} from '@rainbow-me/rainbowkit';
+import {useDisconnect} from "wagmi";
 
 export const Header = () => {
+    const { openConnectModal } = useConnectModal();
+    const { openAccountModal } = useAccountModal();
+    const { openChainModal } = useChainModal();
+    const {disconnect} = useDisconnect()
 
     return (
         <div className={styles.header}>
-            <span className={styles.connect}>connect</span>
+            {openConnectModal && (
+                <span onClick={openConnectModal} className={styles.connect}>connect</span>
+            )}
+
+            {
+                !openConnectModal &&
+                <span
+                    className={styles.connect}
+                    onClick={() => disconnect?.()}
+                >
+                    disconnect
+                </span>
+            }
+
+
+
+            {/*{openAccountModal && (*/}
+            {/*    <button onClick={openAccountModal} type="button">*/}
+            {/*        Open Account Modal*/}
+            {/*    </button>*/}
+            {/*)}*/}
+
+            {/*{openChainModal && (*/}
+            {/*    <button onClick={openChainModal} type="button">*/}
+            {/*        Open Chain Modal*/}
+            {/*    </button>*/}
+            {/*)}*/}
+            {/*<ConnectButton />*/}
             <SocialLinks className={styles.links}/>
             <Marketplaces  className={styles.marketplaces}/>
             <div className={styles.openMenu}/>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,60 @@ import '../styles/globals.scss'
 import 'swiper/css';
 import {AppProps} from 'next/app';
 import Head from "next/head";
-import { Provider } from 'react-redux';
-import { createWrapper } from 'next-redux-wrapper';
-import { store } from '../store/store';
+import {Provider} from 'react-redux';
+import {createWrapper} from 'next-redux-wrapper';
+import {store} from '../store/store';
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+    connectorsForWallets,
+    getDefaultWallets,
+    RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import {
+    chain,
+    configureChains,
+    createClient,
+    WagmiConfig,
+} from 'wagmi';
+import {publicProvider} from 'wagmi/providers/public';
+import {InjectedConnector} from "wagmi/connectors/injected";
+import {WalletConnectConnector} from "wagmi/connectors/walletConnect";
+import {
+    metaMaskWallet,
+    walletConnectWallet
+} from '@rainbow-me/rainbowkit/wallets';
+
+const {chains, provider} = configureChains(
+    [chain.mainnet],
+    [
+        publicProvider()
+    ]
+);
+
+const connectors = connectorsForWallets([
+    {
+        groupName: 'All',
+        wallets: [
+            metaMaskWallet({chains}),
+            walletConnectWallet({chains})
+        ]
+    }
+]);
+
+const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    // connectors: [
+    //     new InjectedConnector({chains}),
+    //     new WalletConnectConnector({
+    //         chains,
+    //         options: {
+    //             qrcode: true,
+    //         },
+    //     }),
+    // ],
+    provider
+})
 
 function App({Component, pageProps}: AppProps) {
     return <>
@@ -12,7 +63,11 @@ function App({Component, pageProps}: AppProps) {
             <title>Pain</title>
         </Head>
         <Provider store={store}>
-            <Component {...pageProps} />
+            <WagmiConfig client={wagmiClient}>
+                <RainbowKitProvider chains={chains} initialChain={chain.mainnet}>
+                    <Component {...pageProps} />
+                </RainbowKitProvider>
+            </WagmiConfig>
         </Provider>
     </>
 }
