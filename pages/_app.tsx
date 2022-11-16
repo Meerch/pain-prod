dc96e68237591c29e8eb9de929492db763c71238
Meerch <khukhriansk333@gmail.com>
2022-11-16 17:29:50 +0300

    try fix error by scripts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -92,8 +92,8 @@ function App({Component, pageProps}: AppProps) {
             <meta name="msapplication-TileColor" content="#da532c"/>
             <meta name="theme-color" content="#ffffff"/>
             <title>Pain</title>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js" async/>
-            <script src="https://cdn.jsdelivr.net/npm/vanta/dist/vanta.waves.min.js" async/>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js" defer/>
+            <script src="https://cdn.jsdelivr.net/npm/vanta/dist/vanta.waves.min.js" defer/>
         </Head>
 
         <Provider store={store}>
